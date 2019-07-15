ea4013033b7aa1d53beebc2b924f0e5dca03721e
Dilip Mariya <dilip@acquaintsoft.com>
2019-07-15 18:03:02 +0530

    Initial-Commit

---
+++ MediaWatermark/MediaWatermark.h
@@ -0,0 +1,19 @@
+//
+//  MediaWatermark.h
+//  MediaWatermark
+//
+//  Created by AGC on 15/07/19.
+//  Copyright © 2019 AGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MediaWatermark.
+FOUNDATION_EXPORT double MediaWatermarkVersionNumber;
+
+//! Project version string for MediaWatermark.
+FOUNDATION_EXPORT const unsigned char MediaWatermarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaWatermark/PublicHeader.h>
+
+
